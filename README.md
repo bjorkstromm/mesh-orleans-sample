414d0d73c3a1e977de1667a977fc2e153645b992
Martin Björkström <martin.bjorkstrom@gmail.com>
2018-10-02 23:55:36 +0300

    Initial code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Azure Service Fabric Mesh Orleans Sample
+
+Orleans Sample originally from [Orleans docker-aspnet-core sample](https://github.com/dotnet/orleans/tree/master/Samples/2.0/docker-aspnet-core)
---
+++ Silo/Program.cs
@@ -0,0 +1,58 @@
+﻿using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Silo
+{
+    public class Program
+    {
+        private static ISiloHost silo;
+        private static readonly ManualResetEvent siloStopped = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            // TODO replace with your connection string
+            const string connectionString = "ENTER_CONNECTION_STRING";
+            silo = new SiloHostBuilder()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "orleans-service-fabric-mesh";
+                    options.ServiceId = "AspNetSampleApp";
+                })
+                .UseAzureStorageClustering(options => options.ConnectionString = connectionString)
+                .ConfigureEndpoints(siloPort: 20005, gatewayPort: 20004, listenOnAnyHostAddress: true)
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(ValueGrain).Assembly).WithReferences())
+                .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Warning).AddConsole())
+                .Build();
+
+            Task.Run(StartSilo);
+
+            AssemblyLoadContext.Default.Unloading += context =>
+            {
+                Task.Run(StopSilo);
+                siloStopped.WaitOne();
+            };
+
+            siloStopped.WaitOne();
+        }
+
+        private static async Task StartSilo()
+        {
+            await silo.StartAsync();
+            Console.WriteLine("Silo started");
+        }
+
+        private static async Task StopSilo()
+        {
+            await silo.StopAsync();
+            Console.WriteLine("Silo stopped");
+            siloStopped.Set();
+        }
+    }
+}
